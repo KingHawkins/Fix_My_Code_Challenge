f287063766b3dd8bfb62ae49547455d7f10edd10
KingHawkins <kinghawkins62@gmail.com>
2023-02-25 12:16:03 +0300

    adding all

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -12,9 +12,12 @@ def fizzbuzz(n):
     multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
+    if n < 1:
+        return
+
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 15 == 0):
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
@@ -31,6 +34,9 @@ if __name__ == '__main__':
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    #number = int(sys.argv[1])
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except:
+        pass
